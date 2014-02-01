e27eb1ccb629b7f1bd2a2d78bbeaa0608def1d1e
root <tjcorley30@hotmail.com>
2014-02-01 02:51:32 +0000

    fixed typo

--- login.py
+++ login.py
@@ -128,10 +128,10 @@ class LoginManager(object):
 		            _class['grade_values'].insert(6,"")
 		            _class['grade_values'].insert(7,"")
 		#I could have a way of determing the period, but I am just going to set it lol...
-		if len(period_list) > 6:
+		if len(period_list) < 6:
 			quarter_index = 3
 		else:
-			quarter_index = 5
+			quarter_index = 4
 		#appends other things we need for displaying grades
 		self.final_list.append({'classes':all_class_list})
 		self.final_list.append({'periods':period_list})
